693904a904955af24ac5c89f2a3caa59ae0defd8
Michael Lynch <mtlynch@users.noreply.github.com>
2022-10-27 09:47:22 -0400

    Add basic HTTP server (#2)
    * Add basic HTTP server
    
    * Fix env variable

--- cmd/screenjournal/main.go
+++ cmd/screenjournal/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+
+	gorilla "github.com/mtlynch/gorilla-handlers"
+
+	"github.com/mtlynch/screenjournal/v2/handlers"
 )
 
 func main() {
 	log.Print("Starting screenjournal server")
 
-	log.Printf("nothing implemented - exiting!")
+	h := gorilla.LoggingHandler(os.Stdout, handlers.New().Router())
+	if os.Getenv("SJ_BEHIND_PROXY") != "" {
+		h = gorilla.ProxyIPHeadersHandler(h)
+	}
+	http.Handle("/", h)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4001"
+	}
+	log.Printf("Listening on %s", port)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/mtlynch/screenjournal/v2
 
 go 1.19
+
+require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/mtlynch/gorilla-handlers v1.5.2 // indirect
+)

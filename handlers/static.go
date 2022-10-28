d970ea5c66cb559bee331df43bd9939e77a5e807
Michael Lynch <mtlynch@users.noreply.github.com>
2022-10-28 14:46:41 -0400

    Add Bootstrap CSS and multiple HTML page templates (#5)

---
+++ handlers/static.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+	"time"
+)
+
+// serveStaticResource serves any static file under the ./static directory.
+func serveStaticResource() http.HandlerFunc {
+	const staticRootDir = "./static"
+	fs := http.FileServer(http.Dir(staticRootDir))
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Set cache headers
+		if mt, ok := lastModTime(path.Join(staticRootDir, r.URL.Path)); ok {
+			etag := "\"" + strconv.FormatInt(mt.UnixMilli(), 10) + "\""
+			w.Header().Set("Etag", etag)
+			w.Header().Set("Cache-Control", "max-age=3600")
+		}
+
+		fs.ServeHTTP(w, r)
+	}
+}
+
+func lastModTime(path string) (time.Time, bool) {
+	file, err := os.Open(path)
+	if err != nil {
+		return time.Time{}, false
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Printf("failed to close file handle for %s: %v", path, err)
+		}
+	}()
+
+	stat, err := file.Stat()
+	if err != nil {
+		return time.Time{}, false
+	}
+	return stat.ModTime(), true
+}
--- handlers/views.go
+++ handlers/views.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"embed"
 	"net/http"
 	"path"
 	"text/template"
@@ -19,18 +20,38 @@ func (s Server) indexGet() http.HandlerFunc {
 	}
 }
 
+func (s Server) aboutGet() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := renderTemplate(w, "about.html", struct {
+			Title string
+		}{
+			Title: "About ScreenJournal",
+		}, template.FuncMap{}); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (s Server) signUpGet() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := renderTemplate(w, "sign-up.html", struct {
+			Title string
+		}{
+			Title: "Sign Up",
+		}, template.FuncMap{}); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+//go:embed templates
+var templatesFS embed.FS
+
 func renderTemplate(w http.ResponseWriter, templateFilename string, templateVars interface{}, funcMap template.FuncMap) error {
-	const templatesRootDir = "./templates"
-	const baseTemplate = "base"
-	const baseTemplateFilename = "base.html"
-
-	templateFiles := []string{}
-	templateFiles = append(templateFiles, path.Join(templatesRootDir, templateFilename))
-	templateFiles = append(templateFiles, path.Join(templatesRootDir, baseTemplateFilename))
-
-	t := template.Must(template.New(templateFilename).Funcs(funcMap).
-		ParseFiles(templateFiles...))
-	if err := t.ExecuteTemplate(w, baseTemplate, templateVars); err != nil {
+	t := template.Must(template.ParseFS(templatesFS, "templates/layouts/*.html", "templates/partials/*.html", path.Join("templates/pages", templateFilename))).Funcs(funcMap)
+	if err := t.ExecuteTemplate(w, "base", templateVars); err != nil {
 		return err
 	}
 	return nil

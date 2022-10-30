bc2471d55653eb822e97c98bf59d1a2b6b877b85
Michael Lynch <git@mtlynch.io>
2022-10-30 09:10:41 -0400

    Add e2e test

---
+++ e2e/reviews.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+import { login } from "./helpers/login.js";
+
+test("adds a new rating and fills all fields", async ({ page }) => {
+  await login(page);
+
+  await page.locator("data-test-id=add-rating").click();
+
+  await page
+    .locator("#media-title")
+    .fill("Eternal Sunshine of the Spotless Mind");
+
+  await page.locator("#rating-select").selectOption({ label: "10" });
+
+  await page.locator("#watched-date").fill("2022-10-30");
+
+  await page.locator("#blurb").fill("My favorite movie!");
+
+  await page.locator("form input[type='submit']").click();
+
+  await expect(page).toHaveURL("/reviews");
+});

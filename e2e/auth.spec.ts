abde76435854a5a9fcc272db6d0265960b0d7f23
Michael Lynch <git@mtlynch.io>
2022-10-30 08:54:20 -0400

    Fix login test

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -6,8 +6,8 @@ test("logs in", async ({ page }) => {
   await page.locator("data-test-id=sign-in-btn").click();
 
   await expect(page).toHaveURL("/login");
-  await page.locator("form input[type='username']").fill("dummyuser");
-  await page.locator("form input[type='password']").fill("dummypass");
+  await page.locator("id=username").fill("dummyuser");
+  await page.locator("id=password").fill("dummypass");
   await page.locator("form input[type='submit']").click();
 
   await expect(page).toHaveURL("/reviews");

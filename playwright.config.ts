ba0854ccdebb4cbca4a36088cf0ac8ba8caaab53
Michael Lynch <git@mtlynch.io>
2022-11-01 21:27:10 -0400

    Reduce to 1 worker in Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ const config: PlaywrightTestConfig = {
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   reporter: "html",
   use: {
     baseURL: "http://localhost:6001",

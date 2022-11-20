2423d44a497fa29e585dbd212b126d2a2238a2b0
Michael Lynch <git@mtlynch.io>
2022-11-20 11:36:33 -0500

    Only show search results that are fully populated

--- metadata/search.go
+++ metadata/search.go
@@ -1,6 +1,8 @@
 package metadata
 
-import "github.com/mtlynch/screenjournal/v2/handlers/parse"
+import (
+	"github.com/mtlynch/screenjournal/v2/handlers/parse"
+)
 
 func (f tmdbFinder) Search(query string) (MovieSearchResults, error) {
 	tmdbResults, err := f.tmdbAPI.SearchMovie(query, map[string]string{
@@ -10,23 +12,32 @@ func (f tmdbFinder) Search(query string) (MovieSearchResults, error) {
 		return MovieSearchResults{}, err
 	}
 	results := MovieSearchResults{
-		Matches:      make([]MovieSearchResult, len(tmdbResults.Results)),
+		Matches:      []MovieSearchResult{},
 		Page:         tmdbResults.Page,
 		TotalPages:   tmdbResults.TotalPages,
 		TotalResults: tmdbResults.TotalResults,
 	}
 
-	for i, match := range tmdbResults.Results {
+	for _, match := range tmdbResults.Results {
+		// Exclude results that are not sufficiently populated.
+		if match.ReleaseDate == "" {
+			continue
+		}
+		if match.PosterPath == "" {
+			continue
+		}
+
 		tmdbID, err := parse.TmdbID(match.ID)
 		if err != nil {
 			return MovieSearchResults{}, err
 		}
-		results.Matches[i] = MovieSearchResult{
+		results.Matches = append(results.Matches, MovieSearchResult{
 			TmdbID:      tmdbID,
 			Title:       match.Title,
 			ReleaseDate: match.ReleaseDate,
 			PosterPath:  match.PosterPath,
-		}
+		})
 	}
+
 	return results, nil
 }

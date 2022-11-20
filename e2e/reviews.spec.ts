4b54ecf66a0d8ca7f52087faa34571064cd8c83a
Michael Lynch <git@mtlynch.io>
2022-11-20 11:01:10 -0500

    Refactor to custom element

--- e2e/reviews.spec.ts
+++ e2e/reviews.spec.ts
@@ -12,12 +12,15 @@ test("adds a new rating and fills in only required fields", async ({
 }) => {
   await page.locator("data-test-id=add-rating").click();
 
-  await page.locator("#media-title").fill("slow lear");
+  await page.locator("title-search #media-title").fill("slow lear");
   const matchingTitle = await page.locator(
     "#search-results-list li:first-child span"
   );
   await expect(matchingTitle).toHaveText("Slow Learners (2015)");
   await matchingTitle.click();
+  await expect(page.locator("title-search #media-title")).toHaveValue(
+    "Slow Learners"
+  );
 
   await page.locator("#rating-select").selectOption({ label: "3" });
 
@@ -44,7 +47,7 @@ test("adds a new rating and fills in only required fields", async ({
 test("adds a new rating and fills all fields", async ({ page }) => {
   await page.locator("data-test-id=add-rating").click();
 
-  await page.locator("#media-title").fill("eternal sunshine");
+  await page.locator("title-search #media-title").fill("eternal sunshine");
   const matchingTitle = await page.locator(
     "#search-results-list li:first-child span"
   );
@@ -52,6 +55,9 @@ test("adds a new rating and fills all fields", async ({ page }) => {
     "Eternal Sunshine of the Spotless Mind (2004)"
   );
   await matchingTitle.click();
+  await expect(page.locator("title-search #media-title")).toHaveValue(
+    "Eternal Sunshine of the Spotless Mind"
+  );
 
   await page.locator("#rating-select").selectOption({ label: "10" });
 
@@ -84,12 +90,15 @@ test("adds a new rating and fills all fields", async ({ page }) => {
 test("adds a new rating and edits the details", async ({ page }) => {
   await page.locator("data-test-id=add-rating").click();
 
-  await page.locator("#media-title").fill("something about ma");
+  await page.locator("title-search #media-title").fill("something about ma");
   const matchingTitle = await page.locator(
     "#search-results-list li:first-child span"
   );
   await expect(matchingTitle).toHaveText("There's Something About Mary (1998)");
   await matchingTitle.click();
+  await expect(page.locator("title-search #media-title")).toHaveValue(
+    "There's Something About Mary"
+  );
 
   await page.locator("#rating-select").selectOption({ label: "10" });
 
@@ -137,12 +146,15 @@ test("adds a new rating and edits the details", async ({ page }) => {
 test("adds a new rating and cancels the edit", async ({ page }) => {
   await page.locator("data-test-id=add-rating").click();
 
-  await page.locator("#media-title").fill("the matri");
+  await page.locator("title-search #media-title").fill("the matri");
   const matchingTitle = await page.locator(
     "#search-results-list li:first-child span"
   );
   await expect(matchingTitle).toHaveText("The Matrix (1999)");
   await matchingTitle.click();
+  await expect(page.locator("title-search #media-title")).toHaveValue(
+    "The Matrix"
+  );
 
   await page.locator("#rating-select").selectOption({ label: "8" });
   await page.locator("#watched-date").fill("2022-10-29");

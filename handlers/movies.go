0f4f860872fe6ff1aa831bffb5c46ea2fff358a9
Michael Lynch <git@mtlynch.io>
2022-11-25 20:28:48 -0500

    Add logic to update movie metadata

--- handlers/movies.go
+++ handlers/movies.go
@@ -18,5 +18,24 @@ func (s Server) repopulateMoviesGet() http.HandlerFunc {
 		}
 
 		log.Printf("read data from %d reviews", len(rr))
+
+		for _, rev := range rr {
+			log.Printf("updating movie %s with latest metadata", rev.Movie.Title)
+
+			movieInfo, err := s.metadataFinder.GetMovieInfo(rev.Movie.TmdbID)
+			if err != nil {
+				log.Printf("failed to get metadata for %s (tmdb ID=%v): %v", movieInfo.Title, movieInfo.TmdbID, err)
+				http.Error(w, fmt.Sprintf("Failed to retrieve metadata: %v", err), http.StatusInternalServerError)
+				return
+			}
+
+			rev.Movie.Title = movieInfo.Title
+
+			if err := s.store.UpdateMovie(rev.Movie); err != nil {
+				log.Printf("failed to get metadata for %s (tmdb ID=%v): %v", rev.Movie.Title, rev.Movie.TmdbID, err)
+				http.Error(w, fmt.Sprintf("Failed to save updated movie metadata: %v", err), http.StatusInternalServerError)
+				return
+			}
+		}
 	}
 }

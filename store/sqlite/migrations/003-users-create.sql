82c024a79d44ffee4995832e0b824d1710d9e54d
Michael Lynch <git@mtlynch.io>
2022-12-22 22:30:09 -0500

    Tabs to spaces

--- store/sqlite/migrations/003-users-create.sql
+++ store/sqlite/migrations/003-users-create.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
-		email TEXT NOT NULL UNIQUE,
-		password_hash TEXT NOT NULL,
-		created_time TEXT NOT NULL,
-		last_modified_time TEXT NOT NULL
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_time TEXT NOT NULL,
+    last_modified_time TEXT NOT NULL
 );

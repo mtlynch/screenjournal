48e7f80a91f2368465e571943f75fe40f41cf51b
Michael Lynch <git@mtlynch.io>
2022-12-25 17:35:18 -0500

    work in progress

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -93,3 +93,30 @@ test("sign up fails if password is too short", async ({ page }) => {
   await page.locator("form input[type='submit']").click();
   await expect(page).toHaveURL("/sign-up");
 });
+
+test("signs up fails after there's already an admin user", async ({ page }) => {
+  await page.goto("/");
+
+  await page.locator("data-test-id=sign-up-btn").click();
+
+  await expect(page).toHaveURL("/sign-up");
+  await page.locator("id=username").fill("dummyadmin");
+  await page.locator("id=email").fill("admin@example.com");
+  await page.locator("id=password").fill("dummypass");
+  await page.locator("id=password-confirm").fill("dummypass");
+  await page.locator("form input[type='submit']").click();
+
+  await expect(page).toHaveURL("/reviews");
+
+  await page.locator("#account-dropdown").click();
+  await page.locator("#navbar-log-out").click();
+
+  // Attempt to sign up again
+  await expect(page).toHaveURL("/");
+
+  await page.locator("data-test-id=sign-up-btn").click();
+
+  await expect(page).toHaveURL("/sign-up");
+
+  await expect(page.locator("form input[type='submit']")).not.toBeVisible();
+});

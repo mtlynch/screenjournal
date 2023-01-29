ed5966e10bff3372aecb8d2984a82366c3614f76
Michael Lynch <git@mtlynch.io>
2023-01-29 07:55:04 -0500

    Remove unused MessageID field

--- email/send.go
+++ email/send.go
@@ -5,20 +5,15 @@ import (
 	"time"
 )
 
-type (
-	MessageID string
-
-	Message struct {
-		From      mail.Address
-		To        []mail.Address
-		Bcc       []mail.Address
-		Subject   string
-		MessageID MessageID
-		Date      time.Time
-		TextBody  string
-		HtmlBody  string
-	}
-)
+type Message struct {
+	From     mail.Address
+	To       []mail.Address
+	Bcc      []mail.Address
+	Subject  string
+	Date     time.Time
+	TextBody string
+	HtmlBody string
+}
 
 type Sender interface {
 	Send(message Message) error

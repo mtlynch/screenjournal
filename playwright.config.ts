ff8757c6484d753f577827f8befafed54c7db73c
Michael Lynch <git@mtlynch.io>
2023-04-16 08:58:50 -0400

    Disable e2e test retries on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ const config: PlaywrightTestConfig = {
   },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: 1,
   reporter: "html",
   use: {

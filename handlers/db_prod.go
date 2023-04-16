4244ad50bdaaf508afad7534b56fca16ae4d6040
Michael Lynch <git@mtlynch.io>
2023-04-16 08:38:26 -0400

    Fix compile issues

--- handlers/db_prod.go
+++ handlers/db_prod.go
@@ -2,6 +2,16 @@
 
 package handlers
 
+import (
+	"net/http"
+
+	"github.com/mtlynch/screenjournal/v2/store"
+)
+
 func (s *Server) addDevRoutes() {
 	// no-op
+}
+
+func (s Server) getDB(*http.Request) store.Store {
+	return s.store
 }

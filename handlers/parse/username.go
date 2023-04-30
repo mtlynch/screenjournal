b76ffe8d5f8028e4a0105c6e261c524a35a9b731
Michael Lynch <git@mtlynch.io>
2023-04-30 12:42:25 -0400

    Reduce the number of generally reserved words
    Only some of the reserved words need to be reserved globally. The rest should be reserved only in the context of usernames.

--- handlers/parse/username.go
+++ handlers/parse/username.go
@@ -11,10 +11,12 @@ var (
 	ErrInvalidUsername = errors.New("invalid username")
 
 	usernamePattern = regexp.MustCompile(`^[a-zA-Z\.0-9]{2,80}$`)
+
+	reservedUsernames = append(reservedWords, []string{"root", "admin", "add", "edit", "delete", "copy"}...)
 )
 
 func Username(username string) (screenjournal.Username, error) {
-	if isReservedWord(username) {
+	if isWordInSlice(username, reservedUsernames) {
 		return screenjournal.Username(""), ErrInvalidUsername
 	}
 

4c44bfb558f03b1de90783c2144e4b0991240a64
Michael Lynch <git@mtlynch.io>
2023-05-07 12:01:13 -0400

    work in progress

--- store/sqlite/migrations/008-adjust-notifications-add-comments.sql
+++ store/sqlite/migrations/008-adjust-notifications-add-comments.sql
@@ -3,8 +3,8 @@ ALTER TABLE notification_preferences ADD COLUMN comments_on_my_reviews INTEGER;
 
 -- Subscribe everyone to new review notifications by default.
 UPDATE
-  notification_preferences
+    notification_preferences
 SET
-  all_new_comments = 1,
-  comments_on_my_reviews = 1
+    all_new_comments = 1,
+    comments_on_my_reviews = 1
 ;

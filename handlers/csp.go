839ba17d86b93817b60adb8d790f9c44f922c262
Michael Lynch <git@mtlynch.io>
2023-05-14 21:21:21 -0400

    Add ponys implementation

--- handlers/csp.go
+++ handlers/csp.go
@@ -32,6 +32,7 @@ func enforceContentSecurityPolicy(next http.Handler) http.Handler {
 				values: []string{
 					"'self'",
 					"'nonce-" + nonce + "'",
+					"https://cdn.jsdelivr.net",
 				},
 			},
 			{
@@ -56,7 +57,7 @@ func enforceContentSecurityPolicy(next http.Handler) http.Handler {
 		}
 		policy := strings.Join(policyParts, "; ") + ";"
 
-		w.Header().Set("Not-Really-Content-Security-Policy", policy)
+		w.Header().Set("Content-Security-Policy", policy)
 
 		ctx := context.WithValue(r.Context(), contextKeyCSPNonce, nonce)
 		next.ServeHTTP(w, r.WithContext(ctx))

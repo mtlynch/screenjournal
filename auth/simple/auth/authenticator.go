30ed5ef8a81dcd83877ab94823bb701dfe64fc50
Michael Lynch <mtlynch@users.noreply.github.com>
2023-10-07 09:42:44 -0400

    Move public parts of simple auth to a single file (#230)

--- auth/simple/auth/authenticator.go
+++ auth/simple/auth/authenticator.go
@@ -1,32 +1,5 @@
 package auth
 
-import "errors"
-
-type (
-	Authenticator interface {
-		Authenticate(username, password string) error
-	}
-
-	AuthStore interface {
-		ReadPasswordHash(username string) (PasswordHash, error)
-	}
-
-	authenticator struct {
-		store AuthStore
-	}
-)
-
-var (
-	ErrUserNotFound      = errors.New("user not found")
-	ErrIncorrectPassword = errors.New("password does not match stored hash")
-)
-
-func New(store AuthStore) Authenticator {
-	return authenticator{
-		store: store,
-	}
-}
-
 func (a authenticator) Authenticate(username, password string) error {
 	h, err := a.store.ReadPasswordHash(username)
 	if err != nil {

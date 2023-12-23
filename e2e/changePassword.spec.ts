fa9aabfe5515d8987bb1b680d1a9312d10e67a31
Michael Lynch <git@mtlynch.io>
2023-12-23 09:28:32 -0500

    Add e2e test

---
+++ e2e/changePassword.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+import { populateDummyData } from "./helpers/db.js";
+import { loginAsUser, loginAsUserA } from "./helpers/login.js";
+
+test.beforeEach(async ({ page }) => {
+  await populateDummyData(page);
+  await loginAsUserA(page);
+});
+
+test("user can change their password and log in again", async ({ page }) => {
+  await page.getByRole("menuitem", { name: "Account" }).click();
+  await page.getByRole("menuitem", { name: "Security" }).click();
+
+  await expect(page).toHaveURL("/account/security");
+  await page.getByText("Change password").click();
+
+  await expect(page).toHaveURL("/account/change-password");
+  await page.getByLabel("Current Password").fill("password123");
+  await page.getByLabel("New Password").fill("password321");
+  await page.getByLabel("Confirm New Password").fill("password321");
+  await page.getByRole("button", { name: /Change password/i }).click();
+
+  await expect(page).toHaveURL("/account/security");
+
+  await page.getByRole("menuitem", { name: "Account" }).click();
+  await page.getByRole("menuitem", { name: "Log out" }).click();
+
+  await loginAsUser(page, "userA", "password321");
+});
+
+// TODO: Test cancel change
+
+// TODO: Test incorrect password

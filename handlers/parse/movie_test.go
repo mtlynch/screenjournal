0c3e043e3773204f8eea3d7907da2c9bdee3c5b1
Michael Lynch <git@mtlynch.io>
2024-01-14 21:59:36 -0500

    Add unit tests for parsing movie ID

---
+++ handlers/parse/movie_test.go
@@ -0,0 +1,59 @@
+package parse_test
+
+import (
+	"testing"
+
+	"github.com/mtlynch/screenjournal/v2/handlers/parse"
+	"github.com/mtlynch/screenjournal/v2/screenjournal"
+)
+
+func TestMovieIDFromSTring(t *testing.T) {
+	for _, tt := range []struct {
+		description string
+		in          string
+		idExpected  screenjournal.MovieID
+		errExpected error
+	}{
+		{
+			"parses valid movie ID",
+			"5",
+			screenjournal.MovieID(5),
+			nil,
+		},
+		{
+			"rejects 0 as an invalid movie ID",
+			"0",
+			screenjournal.MovieID(0),
+			parse.ErrInvalidMovieID,
+		},
+		{
+			"rejects decimal as an invalid movie ID",
+			"2.4",
+			screenjournal.MovieID(0),
+			parse.ErrInvalidMovieID,
+		},
+		{
+			"rejects non-number as an invalid movie ID",
+			"banana",
+			screenjournal.MovieID(0),
+			parse.ErrInvalidMovieID,
+		},
+		{
+			"rejects negative number as an invalid movie ID",
+			"-5",
+			screenjournal.MovieID(0),
+			parse.ErrInvalidMovieID,
+		},
+	} {
+		t.Run(tt.description, func(t *testing.T) {
+			idActual, err := parse.MovieIDFromString(tt.in)
+
+			if got, want := err, tt.errExpected; got != want {
+				t.Fatalf("err=%v, want=%v", got, want)
+			}
+			if got, want := idActual, tt.idExpected; !got.Equal(want) {
+				t.Errorf("movieID=%v, want=%v", got, want)
+			}
+		})
+	}
+}

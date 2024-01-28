e40a70e6e54437dff2f0d957774a46101e62ebbc
Michael Lynch <git@mtlynch.io>
2024-01-28 17:26:11 -0500

    work in progress

--- handlers/search_test.go
+++ handlers/search_test.go
@@ -1,27 +1,80 @@
 package handlers_test
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/mtlynch/screenjournal/v2/handlers"
+	"github.com/mtlynch/screenjournal/v2/metadata"
+	"github.com/mtlynch/screenjournal/v2/screenjournal"
 	"github.com/mtlynch/screenjournal/v2/store/test_sqlite"
 )
 
 func TestSearch(t *testing.T) {
+	movies := []metadata.MovieInfo{
+		{
+			TmdbID:      screenjournal.TmdbID(38),
+			ImdbID:      screenjournal.ImdbID("tt0338013"),
+			Title:       screenjournal.MediaTitle("Eternal Sunshine of the Spotless Mind"),
+			ReleaseDate: screenjournal.ReleaseDate(mustParseDate("2004-03-19")),
+		},
+	}
 	for _, tt := range []struct {
-		description string
+		description       string
+		sessionToken      string
+		searchQuery       string
+		metadataFinderErr error
+		expectedStatus    int
+		expectedResponse  string
 	}{
 		{
-			description: "valid request with all fields populated and movie information is in local DB",
+			description:    "returns matching search results",
+			sessionToken:   "abc123",
+			searchQuery:    "spo",
+			expectedStatus: http.StatusOK,
+			expectedResponse: `{
+				"matches": [
+					{
+						"tmdbId": 38,
+						"title" "Eternal Sunshine of the Spotless Mind",
+						"releaseDate": "TODO",
+						"posterUrl": ""https://image.tmdb.org/t/p/w92/TODO"
+					}
+				]
+			}`,
+		},
+		{
+			description:    "prohibits search if user is not authenticated",
+			sessionToken:   "",
+			searchQuery:    "spo",
+			expectedStatus: http.StatusUnauthorized,
+		},
+		{
+			description:    "prohibits search if user has an invalid session token",
+			sessionToken:   "invalid-token",
+			searchQuery:    "spo",
+			expectedStatus: http.StatusUnauthorized,
+		},
+		{
+			description:       "returns internal server error if metadata finder fails",
+			sessionToken:      "abc123",
+			searchQuery:       "spo",
+			metadataFinderErr: errors.New("dummy error"),
+			expectedStatus:    http.StatusInternalServerError,
 		},
 	} {
 		t.Run(tt.description, func(t *testing.T) {
 			// TODO: Finish test.
 			dataStore := test_sqlite.New()
 
-			s := handlers.New(nilAuthenticator, nilAnnouncer, nil, dataStore, nil)
+			// TODO: Connect the session manager
+			sessionManager := newMockSessionManager([]mockSessionEntry{})
+
+			// TODO: Connect metadata finder with metadataFinderErr
+
+			s := handlers.New(nilAuthenticator, nilAnnouncer, &sessionManager, dataStore, NewMockMetadataFinder(movies))
 
 			req, err := http.NewRequest("GET", "/api/search?query=foo", nil)
 			if err != nil {

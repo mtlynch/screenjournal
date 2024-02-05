08c038eb7746e6b3a74c7bd44143df5edea4f223
Michael Lynch <git@mtlynch.io>
2024-02-04 22:15:57 -0500

    Move Announcer interface to handlers

--- announce/quiet/quiet.go
+++ announce/quiet/quiet.go
@@ -3,21 +3,20 @@ package quiet
 import (
 	"log"
 
-	"github.com/mtlynch/screenjournal/v2/announce"
 	"github.com/mtlynch/screenjournal/v2/screenjournal"
 )
 
-type announcer struct {
+type Announcer struct {
 }
 
-func New() announce.Announcer {
-	return announcer{}
+func New() Announcer {
+	return Announcer{}
 }
 
-func (a announcer) AnnounceNewReview(r screenjournal.Review) {
+func (a Announcer) AnnounceNewReview(r screenjournal.Review) {
 	log.Printf("skipping announcement of review for %s because no announcer is configured", r.Movie.Title)
 }
 
-func (a announcer) AnnounceNewComment(rc screenjournal.ReviewComment) {
+func (a Announcer) AnnounceNewComment(rc screenjournal.ReviewComment) {
 	log.Printf("skipping announcement of new comment from %s about %s's review of %s because no announcer is configured", rc.Owner, rc.Review.Owner, rc.Review.Movie.Title)
 }
--- handlers/server.go
+++ handlers/server.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/mtlynch/screenjournal/v2/announce"
 	"github.com/mtlynch/screenjournal/v2/handlers/sessions"
 	"github.com/mtlynch/screenjournal/v2/metadata"
 	"github.com/mtlynch/screenjournal/v2/screenjournal"
@@ -14,6 +13,11 @@ import (
 )
 
 type (
+	Announcer interface {
+		AnnounceNewReview(screenjournal.Review)
+		AnnounceNewComment(screenjournal.ReviewComment)
+	}
+
 	SessionManager interface {
 		CreateSession(http.ResponseWriter, context.Context, screenjournal.Username, bool) error
 		SessionFromContext(context.Context) (sessions.Session, error)
@@ -31,7 +35,7 @@ type (
 	Server struct {
 		router         *mux.Router
 		authenticator  Authenticator
-		announcer      announce.Announcer
+		announcer      Announcer
 		sessionManager SessionManager
 		store          store.Store
 		metadataFinder metadata.Finder
@@ -45,7 +49,7 @@ func (s Server) Router() *mux.Router {
 
 // New creates a new server with all the state it needs to satisfy HTTP
 // requests.
-func New(authenticator Authenticator, announcer announce.Announcer, sessionManager SessionManager, store store.Store, metadataFinder metadata.Finder) Server {
+func New(authenticator Authenticator, announcer Announcer, sessionManager SessionManager, store store.Store, metadataFinder metadata.Finder) Server {
 	s := Server{
 		router:         mux.NewRouter(),
 		authenticator:  authenticator,

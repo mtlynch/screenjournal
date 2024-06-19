aa491f4e79717ea0af9a928cf543f84574d92397
Michael Lynch <git@mtlynch.io>
2024-06-19 12:15:25 -0400

    Drop reset-on-success

--- static/js/htmx-settings.js
+++ static/js/htmx-settings.js
@@ -7,29 +7,3 @@ document.body.addEventListener("htmx:beforeSwap", function (evt) {
     evt.detail.shouldSwap = true;
   }
 });
-
-// Inspired by https://stackoverflow.com/a/76134033/90388
-htmx.defineExtension("reset-on-success", {
-  onEvent: function (name, event) {
-    if (name !== "htmx:beforeSwap" || event.detail.isError) {
-      return;
-    }
-
-    const triggeringElt = event.detail.requestConfig.elt;
-    if (
-      !triggeringElt.closest("[hx-reset-on-success]") &&
-      !triggeringElt.closest("[data-hx-reset-on-success]")
-    ) {
-      return;
-    }
-
-    switch (triggeringElt.tagName) {
-      case "TEXTAREA":
-        triggeringElt.value = triggeringElt.defaultValue;
-        break;
-      case "FORM":
-        triggeringElt.reset();
-        break;
-    }
-  },
-});

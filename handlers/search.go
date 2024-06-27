c050f99bbeae2d9551481a3271b10031fc0eb967
Michael Lynch <git@mtlynch.io>
2024-06-26 22:06:00 -0400

    Reimplement title search with htmx

--- handlers/search.go
+++ handlers/search.go
@@ -2,17 +2,20 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 type searchMatch struct {
-	TmdbID      int32  `json:"tmdbId"`
-	Title       string `json:"title"`
-	ReleaseDate string `json:"releaseDate"`
-	PosterURL   string `json:"posterUrl"`
+	TmdbID      int32
+	Title       string
+	ReleaseYear string
+	PosterURL   string
 }
 
 func (s Server) searchGet() http.HandlerFunc {
+	t := template.Must(template.ParseFS(templatesFS, "templates/fragments/search-results.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.Query().Get("query")
 		res, err := s.metadataFinder.Search(query)
@@ -24,14 +27,18 @@ func (s Server) searchGet() http.HandlerFunc {
 		for i, m := range res.Matches {
 			matches[i].TmdbID = m.TmdbID.Int32()
 			matches[i].Title = m.Title
-			matches[i].ReleaseDate = m.ReleaseDate
+			matches[i].ReleaseYear = m.ReleaseDate[0:4]
 			matches[i].PosterURL = "https://image.tmdb.org/t/p/w92" + m.PosterPath
 		}
 
-		respondJSON(w, struct {
-			Matches []searchMatch `json:"matches"`
+		if err := t.Execute(w, struct {
+			Results []searchMatch
 		}{
-			Matches: matches,
-		})
+			Results: matches,
+		}); err != nil {
+			http.Error(w, "Failed to render template", http.StatusInternalServerError)
+			log.Printf("failed to render search results template: %v", err)
+			return
+		}
 	}
 }

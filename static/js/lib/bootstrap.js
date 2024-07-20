fa78972bf10551ad6e09d5809a8cf58b51c9c4e2
Michael Lynch <git@mtlynch.io>
2024-07-20 09:27:35 -0400

    Delete unused functions

--- static/js/lib/bootstrap.js
+++ static/js/lib/bootstrap.js
@@ -5,11 +5,3 @@ export function makeInvisible(el) {
 export function makeVisible(el) {
   el.classList.remove("invisible");
 }
-
-export function hideElement(el) {
-  el.classList.add("d-none");
-}
-
-export function showElement(el) {
-  el.classList.remove("d-none");
-}

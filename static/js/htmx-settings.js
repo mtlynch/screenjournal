57ccce451ae81c3fa6adea6f8d79121a35f209d1
Michael Lynch <git@mtlynch.io>
2024-07-20 20:21:33 -0400

    Use response-targets extension
    It's simpler to use the standard extension and simplify the behavior we need.

--- static/js/htmx-settings.js
+++ static/js/htmx-settings.js
@@ -1,8 +1,12 @@
 /* global htmx */
 
+// Tighten security.
 htmx.config.selfRequestsOnly = true;
 htmx.config.allowScriptTags = false;
 htmx.config.allowEval = false;
+
+// Don't let response-targets override isError.
+htmx.config.responseTargetUnsetsError = false;
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.addEventListener("htmx:beforeSwap", function (evt) {

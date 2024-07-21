edd1a33bfb26c6d91804d95bb481b92e26423cb3
Michael Lynch <git@mtlynch.io>
2024-07-20 20:27:59 -0400

    Delete unused controller code

--- static/js/controllers/common.js
+++ static/js/controllers/common.js
@@ -1,12 +1,3 @@
-export function processPlaintextResponse(response) {
-  if (response.ok) {
-    return response.text();
-  }
-  return response.text().then((error) => {
-    return Promise.reject(error);
-  });
-}
-
 export function processJsonResponse(response) {
   if (response.ok) {
     return response.json();

ae801f080404e677aaf0f242354b1fd0eedd962a
Michael Lynch <git@mtlynch.io>
2024-11-07 21:43:03 -0500

    work in progress

--- e2e/helpers/db.js
+++ e2e/helpers/db.js
@@ -4,6 +4,7 @@ export async function populateDummyData(page) {
   await page.goto("/"); // hack to populate the DB cookie
   const response = await page.goto("/api/debug/db/populate-dummy-data");
   await expect(response?.status()).toBe(200);
+  await page.goto("/");
 }
 
 export function readDbTokenCookie(cookies) {

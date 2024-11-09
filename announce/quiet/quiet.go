b278cc829cd55418b09d902ce34a13af52f03a65
Michael Lynch <git@mtlynch.io>
2024-11-09 13:20:42 -0500

    Fix emails, last few places where we assume movie

--- announce/quiet/quiet.go
+++ announce/quiet/quiet.go
@@ -14,9 +14,16 @@ func New() Announcer {
 }
 
 func (a Announcer) AnnounceNewReview(r screenjournal.Review) {
-	log.Printf("skipping announcement of review for %s because no announcer is configured", r.Movie.Title)
+	log.Printf("skipping announcement of review for %s because no announcer is configured", readMediaTitle(r))
 }
 
 func (a Announcer) AnnounceNewComment(rc screenjournal.ReviewComment) {
-	log.Printf("skipping announcement of new comment from %s about %s's review of %s because no announcer is configured", rc.Owner, rc.Review.Owner, rc.Review.Movie.Title)
+	log.Printf("skipping announcement of new comment from %s about %s's review of %s because no announcer is configured", rc.Owner, rc.Review.Owner, readMediaTitle(rc.Review))
+}
+
+func readMediaTitle(r screenjournal.Review) screenjournal.MediaTitle {
+	if !r.Movie.ID.IsZero() {
+		return r.Movie.Title
+	}
+	return r.TvShow.Title
 }

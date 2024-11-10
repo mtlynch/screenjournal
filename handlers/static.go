a90c0d620c53e4088fa257b757b4bf49922c455c
Michael Lynch <mtlynch@users.noreply.github.com>
2024-11-09 21:55:20 -0500

    Embed static resources in binary (#366)

--- handlers/static.go
+++ handlers/static.go
@@ -1,45 +1,34 @@
 package handlers
 
 import (
-	"log"
+	"embed"
+	"io/fs"
 	"net/http"
-	"os"
-	"path"
 	"strconv"
 	"time"
 )
 
-// serveStaticResource serves any static file under the ./static directory.
+//go:embed static
+var staticFS embed.FS
+
+// serveStaticResource serves any static file under the ./handlers/static
+// directory.
 func serveStaticResource() http.HandlerFunc {
-	const staticRootDir = "./static"
-	fs := http.FileServer(http.Dir(staticRootDir))
+	fSys, err := fs.Sub(staticFS, "static")
+	if err != nil {
+		panic(err)
+	}
+	server := http.FileServer(http.FS(fSys))
+	// Because we embed static files in the Go binary, we just use the first time
+	// this function runs as the last modification time for caching headers.
+	lastModificationTime := time.Now()
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Set cache headers
-		if mt, ok := lastModTime(path.Join(staticRootDir, r.URL.Path)); ok {
-			etag := "\"" + strconv.FormatInt(mt.UnixMilli(), 10) + "\""
-			w.Header().Set("Etag", etag)
-			w.Header().Set("Cache-Control", "max-age=3600")
-		}
+		etag := "\"" + strconv.FormatInt(lastModificationTime.UnixMilli(), 10) + "\""
+		w.Header().Set("Etag", etag)
+		w.Header().Set("Cache-Control", "max-age=3600")
 
-		fs.ServeHTTP(w, r)
+		server.ServeHTTP(w, r)
 	}
-}
-
-func lastModTime(path string) (time.Time, bool) {
-	file, err := os.Open(path)
-	if err != nil {
-		return time.Time{}, false
-	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			log.Printf("failed to close file handle for %s: %v", path, err)
-		}
-	}()
-
-	stat, err := file.Stat()
-	if err != nil {
-		return time.Time{}, false
-	}
-	return stat.ModTime(), true
 }

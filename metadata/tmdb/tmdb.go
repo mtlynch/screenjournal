f29e0bdaf780a7f90d08ddda526325599785a128
Michael Lynch <git@mtlynch.io>
2026-02-06 19:49:26 +0000

    Replace go-tmdb library with direct TMDB API calls using net/http
    Co-authored-by: Shelley <shelley@exe.dev>

--- metadata/tmdb/tmdb.go
+++ metadata/tmdb/tmdb.go
@@ -1,15 +1,139 @@
 package tmdb
 
 import (
-	tmdbWrapper "github.com/ryanbradynd05/go-tmdb"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
 )
 
+// Response types for TMDB API.
+
+type MovieResponse struct {
+	Title       string `json:"title"`
+	ImdbID      string `json:"imdb_id"`
+	ReleaseDate string `json:"release_date"`
+	PosterPath  string `json:"poster_path"`
+}
+
+type TvResponse struct {
+	Name         string     `json:"name"`
+	FirstAirDate string     `json:"first_air_date"`
+	PosterPath   string     `json:"poster_path"`
+	Seasons      []TvSeason `json:"seasons"`
+}
+
+type TvSeason struct {
+	SeasonNumber int    `json:"season_number"`
+	Name         string `json:"name"`
+	EpisodeCount int    `json:"episode_count"`
+	AirDate      string `json:"air_date"`
+}
+
+type TvExternalIDs struct {
+	ImdbID string `json:"imdb_id"`
+}
+
+type MovieSearchResult struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	ReleaseDate string `json:"release_date"`
+	PosterPath  string `json:"poster_path"`
+}
+
+type MovieSearchResults struct {
+	Results []MovieSearchResult `json:"results"`
+}
+
+type TvSearchResult struct {
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	FirstAirDate string `json:"first_air_date"`
+	PosterPath   string `json:"poster_path"`
+}
+
+type TvSearchResults struct {
+	Results []TvSearchResult `json:"results"`
+}
+
 type tmdbAPI interface {
-	GetMovieInfo(int, map[string]string) (*tmdbWrapper.Movie, error)
-	GetTvInfo(int, map[string]string) (*tmdbWrapper.TV, error)
-	GetTvExternalIds(int, map[string]string) (*tmdbWrapper.TvExternalIds, error)
-	SearchMovie(query string, options map[string]string) (*tmdbWrapper.MovieSearchResults, error)
-	SearchTv(query string, options map[string]string) (*tmdbWrapper.TvSearchResults, error)
+	GetMovieInfo(id int) (*MovieResponse, error)
+	GetTvInfo(id int) (*TvResponse, error)
+	GetTvExternalIds(id int) (*TvExternalIDs, error)
+	SearchMovie(query string) (*MovieSearchResults, error)
+	SearchTv(query string) (*TvSearchResults, error)
+}
+
+type apiClient struct {
+	apiKey     string
+	httpClient *http.Client
+	baseURL    string
+}
+
+func newAPIClient(apiKey string) *apiClient {
+	return &apiClient{
+		apiKey:     apiKey,
+		httpClient: &http.Client{Timeout: 10 * time.Second},
+		baseURL:    "https://api.themoviedb.org/3",
+	}
+}
+
+func (c *apiClient) get(rawURL string, result interface{}) error {
+	resp, err := c.httpClient.Get(rawURL)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = resp.Body.Close() }()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("TMDB API returned status %d", resp.StatusCode)
+	}
+	return json.NewDecoder(resp.Body).Decode(result)
+}
+
+func (c *apiClient) GetMovieInfo(id int) (*MovieResponse, error) {
+	u := fmt.Sprintf("%s/movie/%d?api_key=%s", c.baseURL, id, c.apiKey)
+	var result MovieResponse
+	if err := c.get(u, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *apiClient) GetTvInfo(id int) (*TvResponse, error) {
+	u := fmt.Sprintf("%s/tv/%d?api_key=%s", c.baseURL, id, c.apiKey)
+	var result TvResponse
+	if err := c.get(u, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *apiClient) GetTvExternalIds(id int) (*TvExternalIDs, error) {
+	u := fmt.Sprintf("%s/tv/%d/external_ids?api_key=%s", c.baseURL, id, c.apiKey)
+	var result TvExternalIDs
+	if err := c.get(u, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *apiClient) SearchMovie(query string) (*MovieSearchResults, error) {
+	u := fmt.Sprintf("%s/search/movie?api_key=%s&query=%s&include_adult=false", c.baseURL, c.apiKey, url.QueryEscape(query))
+	var result MovieSearchResults
+	if err := c.get(u, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (c *apiClient) SearchTv(query string) (*TvSearchResults, error) {
+	u := fmt.Sprintf("%s/search/tv?api_key=%s&query=%s&include_adult=false", c.baseURL, c.apiKey, url.QueryEscape(query))
+	var result TvSearchResults
+	if err := c.get(u, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
 }
 
 type Finder struct {
@@ -17,10 +141,7 @@ type Finder struct {
 }
 
 func New(apiKey string) (Finder, error) {
-	tmbdAPI := tmdbWrapper.Init(tmdbWrapper.Config{
-		APIKey: apiKey,
-	})
-	return NewWithAPI(tmbdAPI), nil
+	return NewWithAPI(newAPIClient(apiKey)), nil
 }
 
 func NewWithAPI(api tmdbAPI) Finder {
